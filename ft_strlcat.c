4a0b6e49d328fb6c55ce0464711cbddca4d19431
Сергей Горбачев <levensta@et-g4.kzn.21-school.ru>
2020-11-11 20:20:49 +0300

    final + fix strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: levensta <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 14:58:03 by levensta          #+#    #+#             */
-/*   Updated: 2020/11/10 23:38:55 by levensta         ###   ########.fr       */
+/*   Updated: 2020/11/11 20:18:49 by levensta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,20 @@ size_t		ft_strlcat(char *dest, const char *source, size_t n)
 {
 	size_t i;
 	size_t j;
-	size_t source_size;
-	size_t prikol;
+	size_t dest_size;
 
 	i = 0;
-	j = ft_strlen(dest);
-	source_size = ft_strlen((char *)source);
-	// if (n == 0)
-	// {
-	// 	dest[j] = '\0';
-	// 	return (0);
-	// }
-	source_size += j;
-	while (source[i] != '\0' && j < n - 1)
+	dest_size = ft_strlen(dest);
+	j = dest_size;
+	if (n == 0)
+		return (ft_strlen(source));
+	while (j < n - 1 && source[i] != '\0')
 	{
 		dest[j] = source[i];
 		i++;
 		j++;
 	}
 	dest[j] = '\0';
-	prikol = (j < n) ? j : n;
-	return (source_size + prikol);
-}
-
-int main ()
-{
-	char *s1 = ft_strdup("");
-	char s2[] = "toto";
-	ft_strlcat(s1,s2,0);
-	printf("|%s|", s1);
-	return (0);
+	dest_size = (dest_size < n) ? dest_size : n;
+	return (ft_strlen(source) + dest_size);
 }
